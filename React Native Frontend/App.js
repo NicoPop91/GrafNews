3a23d5de1f2eaa09e5af4d4d6da774f250291ca7
Blaupanzer <eacd6@DESKTOP-2I5AM0S.fritz.box>
2018-03-02 21:39:24 +0100

    Added React Native App (initital commit)

---
+++ React Native Frontend/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app/index';
+
+class Project extends Component {
+  render() {
+    return <App />;
+  }
+}
+
+export default Project;
+
+AppRegistry.registerComponent('FirstLookReactNavigation', () => App);
---
+++ React Native Frontend/app/screens/settings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet, TextInput, Alert, Dimensions, Image, ScrollView, RefreshControl, FlatList, Linking, TouchableOpacity, ActivityIndicator} from 'react-native';
+import { List, ListItem, SearchBar, SocialIcon, Header, ButtonGroup, Button } from "react-native-elements";
+const Orientation = require('../config/orientation.js');
+
+export default class Settings extends Component {
+  render() {
+    return <Text>Settings</Text>;
+  }
+}
