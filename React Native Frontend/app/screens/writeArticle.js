437084cd70e6117051009d075925dba72dcf0234
Blaupanzer <eacd6@DESKTOP-2I5AM0S.fritz.box>
2018-03-17 23:25:29 +0100

    New design when writing articles

--- React Native Frontend/app/screens/writeArticle.js
+++ React Native Frontend/app/screens/writeArticle.js
@@ -21,25 +21,75 @@ import gql from 'graphql-tag';
 import DatePicker from 'react-native-datepicker';
 const { createApolloFetch } = require('apollo-fetch');
 var moment = require('moment');
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+var Category = t.enums({
+  general: 'General',
+  business: 'Business',
+  entertainment: 'Entertainment',
+  health: 'Health',
+  science: 'Science',
+  sports: 'Sports',
+  technology: 'Technology'
+});
+
+var Language = t.enums({
+  de: 'German',
+  en: 'English'
+});
+
+var Country = t.enums({
+  de: 'Germany',
+  us: 'United States',
+  gb: 'Great Britain'
+});
+
+Form.stylesheet.dateValue.normal.borderColor = '#d0d2d3';
+Form.stylesheet.dateValue.normal.borderRadius = 3;
+Form.stylesheet.dateValue.normal.borderWidth = 1;
+
+var Article = t.struct({
+  title: t.String,
+  text: t.String,
+  author: t.String,
+  date: t.Date,
+  category: Category,
+  language: t.maybe(Language),
+  country: t.maybe(Country),
+  url: t.maybe(t.String),
+  urlToImage: t.maybe(t.String)
+});
+
+const categories = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
+const countries = ['de', 'us', 'gb'];
+const languages = ['de', 'en'];
+const label_categories = ['General', 'Business', 'Entertainment', 'Health', 'Science', 'Sports', 'Technology'];
+const label_countries = ['Germany', 'United States', 'Great Britain'];
+const label_languages = ['German', 'English'];
+
+const options = {
+  fields: {
+    title: {
+      error: 'Please provide a title'
+    },
+    text: {
+      error: 'Please provide a text'
+    },
+    author: {
+      error: 'Please provide your name',
+    },
+  },
+};
 
 export default class WriteArticle extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: null,
-      text: null,
-      image: null,
-      showPhotoGallery: false,
-      photoArray: [],
-      author: null,
-      url: null,
-      language: 'de',
-      category: 'general',
-      country: 'de',
-      photos: null,
-      date: null,
-      activeMenu: null
+      value:null
     };
+    this.onPress = this.onPress.bind(this);
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -60,8 +110,8 @@ export default class WriteArticle extends Component {
     };
   };
 
-  sumbit = () => {
-    if(this.state.text !== null){
+  sumbit = (value) => {
+    /*if(this.state.text !== null){
       var text = this.state.text.replace(/\n/g, " ");
     }
     var v1 = this.state.date;
@@ -74,13 +124,13 @@ export default class WriteArticle extends Component {
       date = v2[0]+"T"+hour+":"+v3[1]+":"+v3[2]+".000Z";
     } else {
       date = v2[0]+"T"+hour+":"+v3[1]+":"+v3[2]+".000Z";
-    }
-    console.log("Trying to submit " + this.state.author + "; " + this.state.title + "; " + text + "; " + this.state.url + "; " + this.state.image + "; " + this.state.category + "; " + this.state.language + "; " + this.state.country + "; " + date );
+    }*/
+    console.log("Trying to submit " + value.author + "; " + value.title + "; " + value.text + "; " + value.url + "; " + value.urlToImage + "; " + value.category + "; " + value.language + "; " + value.country + "; " + value.date.toISOString() );
     const fetch = createApolloFetch({
       uri: 'http://9p7wpw3ppo75fifx.myfritz.net:4000/graphql',
     });
     fetch({
-      query: 'mutation {addArticle(author:"' + this.state.author + '" title:"' + this.state.title + '" description:"' + text +  '" url:"' + this.state.url + '" urlToImage:"' + this.state.image + '" category:"' + this.state.category + '" language:"' + this.state.language + '" country:"' + this.state.country + '" publishedAt:"' + date + '") { id }}'
+      query: 'mutation {addArticle(author:"' + value.author + '" title:"' + value.title + '" description:"' + value.text +  '" url:"' + value.url + '" urlToImage:"' + value.urlToImage + '" category:"' + value.category + '" language:"' + value.language + '" country:"' + value.country + '" publishedAt:"' + value.date.toISOString() + '") { id }}'
     })
     .then(respnse => {
       console.log('Article was generated with id ', respnse.data)
@@ -91,192 +141,43 @@ export default class WriteArticle extends Component {
     });
   };
 
-  getPhotosFromGallery() {
-    CameraRoll.getPhotos({ first: 1000000 })
-      .then(res => {
-        let photoArray = res.edges;
-        this.setState({ showPhotoGallery: true, photoArray: photoArray })
-      })
+  getInitialState() {
+    return { value: null };
   }
 
-  setActiveMenu = (menu) => {
-    this.setState({activeMenu:menu});
-  }
+  onChange(value) {
+    this.setState({ value });
+  };
+
+  clearForm() {
+    this.setState({ value: null });
+  };
+
+  onPress() {
+    var value = this.refs.form.getValue();
+    if (value) {
+      console.log(value);
+      this.sumbit(value);
+      this.clearForm();
+    }
+  };
 
   render() {
-    const categories = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
-    const countries = ['de', 'us', 'gb'];
-    const languages = ['de', 'en'];
-    const label_categories = ['General', 'Business', 'Entertainment', 'Health', 'Science', 'Sports', 'Technology'];
-    const label_countries = ['Germany', 'United States', 'Great Britain'];
-    const label_languages = ['German', 'English'];
     return (
-      <View>
-        <ScrollView style={{ flexDirection:'column', padding: 20 }}>
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 0, fontSize: 24 }}
-            onChangeText={title => this.setState({ title })}
-            value={this.state.title}
-            placeholderTextColor={"grey"}
-            allowFontScaling={true}
-            autoFocus={true}
-            placeholder={"Title ..."}
-            maxHeight={400}
-            multiline={false}
+      <View style={styles.container2}>
+        <ScrollView>
+          <Form 
+            ref="form"
+            type={Article}
+            options={options}
+            value={this.state.value}
+            onChange={this.onChange.bind(this)}
           />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 0 }}
-            onChangeText={text => this.setState({ text })}
-            value={this.state.text}
-            placeholderTextColor={"grey"}
-            allowFontScaling={true}
-            placeholder={"Your text starts here ..."}
-            maxHeight={400}
-            multiline={true}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 0}}
-            onChangeText={author => this.setState({ author: author })}
-            value={this.state.author}
-            placeholderTextColor={"grey"}
-            allowFontScaling={true}
-            placeholder={"Author ..."}
-            maxHeight={400}
-            multiline={false}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <View style={{flexDirection:'row'}}>
-            <Button
-              title={"Category"}
-              onPress={() => {this.setActiveMenu('Category')}}
-            />
-            <Button
-              title={"Language"}
-              onPress={() => {this.setActiveMenu('Language')}}
-            />
-            <Button
-              title={"Country"}
-              onPress={() => {this.setActiveMenu('Country')}}
-            />
-          </View>
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 0}}
-            onChangeText={url => this.setState({ url: url })}
-            value={this.state.url}
-            placeholderTextColor={"grey"}
-            allowFontScaling={true}
-            placeholder={"URL ..."}
-            maxHeight={400}
-            multiline={false}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <TextInput
-            style={{ borderColor: "gray"}}
-            onChangeText={image => this.setState({ image: image })}
-            value={this.state.image}
-            placeholderTextColor={"grey"}
-            allowFontScaling={true}
-            placeholder={"URL to image ..."}
-            maxHeight={400}
-            multiline={false}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          {
-            this.state.image !== null ? (
-              <Image
-                source={{ uri: this.state.image}}
-              />
-            ):(
-              null
-            )
-          }
-          <DatePicker
-            style={{width: '100%'}}
-            date={this.state.date}
-            mode="datetime"
-            placeholder="select date"
-            format="YYYY-MM-DD hh:mm:ss a"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            customStyles={{
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0
-              },
-              dateInput: {
-                marginLeft: 36
-              },
-              btnTextConfirm: {
-                color: '#007AFF'
-              }
-            }}
-            onDateChange={(date) => {this.setState({date: date})}}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
-          <Button
-            title={"Submit"}
-            onPress={() => {this.sumbit()}}
-          />
-          <Divider style={{ height: 20, backgroundColor: "transparent" }} />
+          <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableHighlight>
         </ScrollView>
-        <View style={{borderWidth:0, backgroundColor:'grey'}}>
-            { this.state.activeMenu === 'Category' ? (
-              <Picker
-                style={{flex:1, backgroundColor:'transparent'}}
-                selectedValue={this.state.category}
-                onValueChange={(itemValue,itemIndex) => this.setState({category:itemValue})}
-                mode={'dialog'}
-                prompt={'category'}
-              >
-                <Picker.Item label={label_categories[0]} value={categories[0]} />
-                <Picker.Item label={label_categories[1]} value={categories[1]} />
-                <Picker.Item label={label_categories[2]} value={categories[2]} />
-                <Picker.Item label={label_categories[3]} value={categories[3]} />
-                <Picker.Item label={label_categories[4]} value={categories[4]} />
-                <Picker.Item label={label_categories[5]} value={categories[5]} />
-                <Picker.Item label={label_categories[6]} value={categories[6]} />
-              </Picker>
-            ):(
-              null
-            )}
-
-            { this.state.activeMenu === 'Language' ? (
-              <Picker
-                style={{flex:1, backgroundColor:'transparent'}}
-                selectedValue={this.state.language}
-                onValueChange={(itemValue,itemIndex) => this.setState({language:itemValue})}
-                mode={'dialog'}
-                prompt={'language'}
-              >
-                <Picker.Item label={label_languages[0]} value={languages[0]} />
-                <Picker.Item label={label_languages[1]} value={languages[1]} />
-              </Picker>
-            ):(
-              null
-            )}
-
-            { this.state.activeMenu === 'Country' ? (
-              <Picker
-                style={{flex:1, backgroundColor:'transparent'}}
-                selectedValue={this.state.country}
-                onValueChange={(itemValue,itemIndex) => this.setState({country:itemValue})}
-                mode={'dialog'}
-                prompt={'country'}
-              >
-                <Picker.Item label={label_countries[0]} value={countries[0]} />
-                <Picker.Item label={label_countries[1]} value={countries[1]} />
-                <Picker.Item label={label_countries[2]} value={countries[2]} />
-              </Picker>
-            ):(
-              null
-            )}
-          </View>
-      </View>
+      </View>   
     );
   }
 }
@@ -286,5 +187,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  container2: {
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
