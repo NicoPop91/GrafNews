b40864b9e88463e0a1dbf1481d84c65cb477522f
fpulch <31630392+fpulch@users.noreply.github.com>
2018-04-01 22:16:38 +0200

    Masterbranch reorganisiert
    Entschuldigung für das Missgeschick

--- BackendGraphQL/server.js
+++ BackendGraphQL/server.js
@@ -7,21 +7,29 @@ const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 const NewsAPI = require('newsapi');
 //const newsapi = new NewsAPI('188776de06144080885d5b3f324f05e7');
-const newsapi = new NewsAPI('aa8d9ecd389b4f358eb9c411ffa14724');
+let apiKeyIndex = 1;
+let newsapi = new NewsAPI('aa8d9ecd389b4f358eb9c411ffa14724');
 
 const app = express();
 
-
-const MONGO_URI = 'mongodb://192.168.99.100:32809/MyDatabase';
+//const MONGO_URI = 'mongodb://192.168.178.39:27017/NewsDB/';
+const MONGO_URI = 'mongodb://localhost:27017/MyDatabase';
+// const MONGO_URI = 'https://g4qr3mtniplvry1i.myfritz.net:27017/NewsDB';
+// const MONGO_URI = 'mongodb://192.168.99.100:32768/MyDatabase';
+// https://g4qr3mtniplvry1i.myfritz.net:27017/NewsDB
+// const MONGO_URI = 'mongodb://g4qr3mtniplvry1i.myfritz.net:27017/MyDatabase';
+// const MONGO_URI = 'mongodb://192.168.99.100:32809/MyDatabase';
 //const MONGO_URI = 'mongodb://g4qr3mtniplvry1i.myfritz.net:27017/MyDatabase';
 // const MONGO_URI = 'http://9p7wpw3ppo75fifx.myfritz.net:4000/playground';
 if (!MONGO_URI) {
     throw new Error('You must provide a MongoLab URI');
 }
 
-
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useMongoClient: true,
+  /* other options */
+});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
@@ -51,8 +59,8 @@ setInterval(() => {
     categories.forEach((category) => {
         languages.forEach((language) => {
             countries.forEach((country) => {
-                
-                
+
+
                 if (!((country === 'de' && language === 'en') || (country === 'us' && language === 'de') || (country === 'gb' && language === 'de'))) {
                     newsapi.v2.topHeadlines({
                         category: category,
@@ -62,6 +70,7 @@ setInterval(() => {
                         return response.articles;
                     }).then((articles) => {
                         articles.forEach((news) => {
+                            console.log(news);
                             Article.find({
                                     title: news.title
                                 })
@@ -88,11 +97,28 @@ setInterval(() => {
                                     }
                                 });
                         });
+                    }).catch((err)=>{
+                        if(err){
+                            //APIKey 1 ea30eaed8d1d45e3828339efc4a12c08
+                            //APIKey 2 aa8d9ecd389b4f358eb9c411ffa14724
+                            //APIKey 3 bdeb31f66ff14026ae9db0a657aafa42
+                            apiKeyIndex++;
+                            if(apiKeyIndex=4){
+                                newsapi = new NewsAPI('ea30eaed8d1d45e3828339efc4a12c08');
+                                apiKeyIndex=1;
+                            }
+                            if(apiKeyIndex=2){
+                               newsapi = new NewsAPI('aa8d9ecd389b4f358eb9c411ffa14724'); 
+                            }
+                            if(apiKeyIndex=3){
+                                newsapi = new NewsAPI('bdeb31f66ff14026ae9db0a657aafa42');
+                            }
+                        }
                     });
-                    
+
                 }
-                
-                
+
+
             });
         });
     });
